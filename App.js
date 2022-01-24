09db81da99418df7c96a8479d176bf7166ffe90c
JuanAira <juanaira_01@hotmail.com>
2022-01-24 17:28:49 -0300

    Update app.js

--- App.js
+++ App.js
@@ -2,16 +2,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet } from 'react-native';
-import NavigationStack from './src/Navigation/Stack';
 import NavigationTab from './src/Navigation/Tab';
-import NavigationDrawer from './src/Navigation/Drawer';
 
 export default function App() {
   return (
     <NavigationContainer>{
-/*       <NavigationStack />
-      <NavigationTab /> */
-      <NavigationDrawer />
+      <NavigationTab />
     }</NavigationContainer >
   );
 }

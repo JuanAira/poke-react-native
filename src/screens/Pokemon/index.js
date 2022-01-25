9ccd3f786ba84becebb34d0cc1fc3a68af37ed20
juanaira-ml <juan.aira@mercadolibre.com>
2022-01-25 17:51:17 -0300

    Add screen pokemon

--- src/screens/Pokemon/index.js
+++ src/screens/Pokemon/index.js
@@ -1,10 +1,58 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { getPokemonDetailsApi } from '../../api/pokemon';
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import Header from '../../components/Header';
+import Type from '../../components/Type';
+import Stats from '../../components/Stats';
 
-export default function Pokemon() {
+export default function Pokemon(props) {
+  const {
+    navigation,
+    route: { params },
+  } = props;
+
+  const { id } = params;
+
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRighy: () => null,
+      headerLeft: () =>
+        <Icon
+          name="arrow-left"
+          color='#fff'
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={navigation.goBack}
+        />,
+    })
+  }, [navigation, params])
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi({ id });
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [id]);
+
+  if (!pokemon) return null;
+
   return (
-    <View>
-      <Text>Estamos en un Pokemon</Text>
-    </View>
+    <ScrollView>
+      <Header
+        name={pokemon.name}
+        order={pokemon.order}
+        image={pokemon.sprites.other["official-artwork"].front_default}
+        type={pokemon.types[0].type.name}
+      />
+      <Type types={pokemon.types} />
+      <Stats stats={pokemon.stats} />
+    </ScrollView>
   );
 }

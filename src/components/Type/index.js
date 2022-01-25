e7ae9b17b241a379a8559f330f0d12806e0abc69
juanaira-ml <juan.aira@mercadolibre.com>
2022-01-25 17:50:56 -0300

    Add component type

---
+++ src/components/Type/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { map, capitalize } from "lodash";
+import getColorByPokemonType from "../../utils/getColorByPokemonType";
+import { styles } from "./styles";
+
+export default function Type(props) {
+  const { types } = props;
+
+  return (
+    <View style={styles.content}>
+      {map(types, (item, index) => (
+        <View
+          key={index}
+          style={{
+            ...styles.pill,
+            backgroundColor: getColorByPokemonType({ type: item.type.name }),
+          }}
+        >
+          <Text>{capitalize(item.type.name)}</Text>
+        </View>
+      ))}
+    </View>
+  );
+}
+
---
+++ src/components/Type/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  content: {
+    marginTop: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pill: {
+    paddingHorizontal: 30,
+    paddingVertical: 5,
+    borderRadius: 20,
+    marginHorizontal: 10,
+  },
+});

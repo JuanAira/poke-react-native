afb2320934faad1c29a5a852964a9deebfec0621
juanaira-ml <juan.aira@mercadolibre.com>
2022-01-25 17:50:38 -0300

    Add styles

---
+++ src/components/Stats/styles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from '../../utils/constants';
+
+export const styles = StyleSheet.create({
+  content: {
+    paddingHorizontal: 20,
+    marginTop: 40,
+    marginBottom: 80,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    paddingBottom: 5,
+  },
+  block: {
+    flexDirection: "row",
+    paddingVertical: 5,
+  },
+  blockTitle: {
+    width: "30%",
+  },
+  statName: {
+    fontSize: 12,
+    color: COLORS.GRAY,
+  },
+  blockInfo: {
+    width: "70%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  number: {
+    width: "12%",
+    fontSize: 12,
+  },
+  bgBar: {
+    backgroundColor: COLORS.LIGHT_GRAY,
+    width: "88%",
+    height: 5,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+  bar: {
+    height: 5,
+    borderRadius: 20,
+  },
+});
